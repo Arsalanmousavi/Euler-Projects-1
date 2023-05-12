160d5494ee51607aa1b156783af3adc4366e1fe2
Acrow <arsalanmousavi21@gmail.com>
2023-05-12 22:31:07 +0330

    Change variable name

--- Euler1.py
+++ Euler1.py
@@ -1,8 +1,8 @@
 multiples_list = []
 
-for i in range(0, 1000):
-    if i % 3 == 0 or i % 5 == 0:
-        multiples_list.append(i)
+for n in range(0, 1000):
+    if n % 3 == 0 or n % 5 == 0:
+        multiples_list.append(n)
 
 print(multiples_list)
 print(sum(multiples_list))
